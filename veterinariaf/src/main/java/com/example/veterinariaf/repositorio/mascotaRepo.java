02f7b142b7705816d0174322687880f89fed68ea
cristian camilo malaver ayala <cris.000165@gmail.com>
2023-09-07 17:47:23 -0500

    primera parte de la base de datos, creado mascotas,propietario,veterinario, antesedentes y comienzo del registro

---
+++ veterinariaf/src/main/java/com/example/veterinariaf/repositorio/mascotaRepo.java
@@ -0,0 +1,8 @@
+package com.example.veterinariaf.repositorio;
+
+import com.example.veterinariaf.entity.mascota;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface mascotaRepo extends CrudRepository<mascota, Integer> {
+}
